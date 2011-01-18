148c47e1eef29141c015e91a17a0e8a40c21c13b
Arnaud Gelas <arnaud_gelas@hms.harvard.edu>
2011-01-18 16:30:00 -0500

    COMP: make the test compile

--- Code/itkFastMarchingBase.h
+++ Code/itkFastMarchingBase.h
@@ -19,6 +19,14 @@
 #ifndef __itkFastMarchingBase_h
 #define __itkFastMarchingBase_h
 
+#include "itkImage.h"
+#include "itkImageToImageFilter.h"
+
+#include "itkMesh.h"
+#include "itkMeshToMeshFilter.h"
+
+#include "itkPriorityQueueContainer.h"
+
 namespace itk
 {
 template< class TInputDomain,
@@ -29,10 +37,14 @@ class FastMarchingTraits
   {
 public:
   typedef TInputDomain InputDomainType;
-  typedef InputDomainType::Pointer InputDomainPointer;
+  typedef typename InputDomainType::Pointer InputDomainPointer;
+  typedef typename InputDomainType::PixelType InputPixelType;
 
   typedef TNode NodeType;
   typedef TOutputDomain OutputDomainType;
+  typedef typename OutputDomainType::Pointer OutputDomainPointer;
+  typedef typename OutputDomainType::PixelType OutputPixelType;
+  typedef TSuperclass SuperclassType;
 
   // Here concept checking: make sure TValue is scalar!
   };
@@ -40,13 +52,15 @@ public:
 template< unsigned int VDimension, typename TInputPixel, typename TOutputPixel >
 class ImageFastMarchingTraits :
     public FastMarchingTraits<
-      Image< VDimension, TInputPixel >,
+      Image< TInputPixel, VDimension >,
       Index< VDimension >,
-      Image< VDimension, TOutputPixel >,
-      ImageToImageFilter< Image< VDimension, TInputPixel >,
-                          Image< VDimension, TOutputPixel > >
+      Image< TOutputPixel, VDimension >,
+      ImageToImageFilter< Image< TInputPixel, VDimension >,
+                          Image< TOutputPixel, VDimension > >
     >
-  {};
+  {
+
+  };
 
 template< unsigned int VDimension,
           typename TInputPixel,
@@ -59,7 +73,7 @@ class MeshFastMarchingTraits :
       typename TInputMeshTraits::PointIdentifier,
       Mesh< TOutputPixel, VDimension, TOutputMeshTraits >,
       MeshToMeshFilter< Mesh< TInputPixel, VDimension, TInputMeshTraits >,
-                        Mesh< TOutputPixel, VDimension, TOutputMeshTraits > >
+                        Mesh< TOutputPixel, VDimension, TOutputMeshTraits > > >
   {};
 
 
@@ -68,23 +82,34 @@ template< class TTraits >
 class FastMarchingBase : public TTraits::SuperclassType
   {
 public:
-  typedef FastMarchingBase                Self;
-  typedef TSuperclass                 Superclass;
+  typedef TTraits                               Traits;
+  typedef typename Traits::SuperclassType     SuperclassType;
+
+  typedef FastMarchingBase            Self;
+  typedef SuperclassType              Superclass;
   typedef SmartPointer< Self >        Pointer;
   typedef SmartPointer< const Self >  ConstPointer;
 
-  typedef TTraits                               Traits;
-  typedef typename TTraits::InputDomainType     InputDomainType;
-  typedef typename TTraits::InputDomainPointer  InputDomainPointer;
 
-  typedef typename TTraits::OutputDomainType     OutputDomainType;
-  typedef typename TTraits::OutputDomainPointer  OutputDomainPointer;
+  typedef typename Traits::InputDomainType     InputDomainType;
+  typedef typename Traits::InputDomainPointer  InputDomainPointer;
+  typedef typename Traits::InputPixelType      InputPixelType;
 
-  typedef typename TTraits::NodeType NodeType;
+  typedef typename Traits::OutputDomainType     OutputDomainType;
+  typedef typename Traits::OutputDomainPointer  OutputDomainPointer;
+  typedef typename Traits::OutputPixelType      OutputPixelType;
 
-  typedef MinPriorityQueueElementWrapper< NodeType, OutputPixelType, long > PriorityQueueElementType;
-  typedef PriorityQueueContainer< MinPriorityQueueElementWrapper,
-    MinPriorityQueueElementWrapper, OutputPixelType, long > PriorityQueueType;
+  typedef typename Traits::NodeType NodeType;
+  typedef long ElementIdentifier;
+
+  typedef MinPriorityQueueElementWrapper< NodeType,
+    OutputPixelType,
+    ElementIdentifier > PriorityQueueElementType;
+
+  typedef PriorityQueueContainer< PriorityQueueElementType,
+    PriorityQueueElementType,
+    OutputPixelType,
+    ElementIdentifier > PriorityQueueType;
   typedef typename PriorityQueueType::Pointer PriorityQueuePointer;
 
   /** Enum of Fast Marching algorithm point types.
@@ -116,18 +141,19 @@ public:
   virtual void SetTrialNodes( NodeContainerType iNodes ) = 0;
   virtual void AddTrialNode( NodeType iNode, OutputPixelType iValue ) = 0;
 
-  virtual void SetForbiddenNode( NodeType iNode ) = 0;
+  virtual void SetForbiddenNodes( NodeType iNodes ) = 0;
   virtual void AddForbiddenNode( NodeType iNode ) = 0;
 
 
 protected:
 
-  FastMarchingBase();
-  virtual ~FastMarchingBase();
+  FastMarchingBase() {}
+  virtual ~FastMarchingBase() {}
 
   double m_SpeedConstand;
   double m_InverseSpeed;
   double m_NormalizationFactor;
+  OutputPixelType m_StoppingValue;
 
   PriorityQueuePointer m_Heap;
 
@@ -142,6 +168,9 @@ protected:
     {
     try
       {
+      double newProgress = 0.;
+      double oldProgress = 0.;
+
       while( !m_Heap->Empty() )
         {
         PriorityQueueElementType element = m_Heap->Peek();
@@ -168,7 +197,8 @@ protected:
           this->UpdateNeighbors( current_node );
 
           // Send events every certain number of points.
-          newProgress = current_value / m_StoppingValue;
+          newProgress = static_cast< double >( current_value ) /
+            static_cast< double >( m_StoppingValue );
           if ( current_value - oldProgress > 0.01 )
             {
             this->UpdateProgress(newProgress);
--- Testing/itkFastMarchingBaseTest.cxx
+++ Testing/itkFastMarchingBaseTest.cxx
@@ -1,10 +1,72 @@
 #include "itkFastMarchingBase.h"
 
+namespace itk
+{
+template< class TTraits >
+class FastMarchingBaseTestHelper : public FastMarchingBase< TTraits >
+{
+public:
+  typedef FastMarchingBaseTestHelper Self;
+  typedef FastMarchingBase< TTraits > Superclass;
+  typedef SmartPointer< Self > Pointer;
+  typedef SmartPointer< const Self > ConstPointer;
+
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+
+  /** Run-time type information (and related methods). */
+  itkTypeMacro(FastMarchingBaseTestHelper, FastMarchingBase);
+
+  typedef typename Superclass::NodeContainerType  NodeContainerType;
+  typedef typename Superclass::NodeType           NodeType;
+
+  typedef typename Superclass::OutputPixelType  OutputPixelType;
+  typedef typename Superclass::LabelType        LabelType;
+
+  void SetAliveNodes( NodeContainerType iNodes )
+    { (void) iNodes; }
+  virtual void AddAliveNode( NodeType iNode, OutputPixelType iValue )
+    { (void) iNode; (void) iValue; }
+
+  void SetTrialNodes( NodeContainerType iNodes )
+    { (void) iNodes; }
+  void AddTrialNode( NodeType iNode, OutputPixelType iValue )
+    { (void) iNode; (void) iValue; }
+
+  virtual void SetForbiddenNodes( NodeType iNodes )
+    { (void) iNodes; }
+  virtual void AddForbiddenNode( NodeType iNode )
+    { (void) iNode; }
+
+protected:
+  FastMarchingBaseTestHelper() {}
+  ~FastMarchingBaseTestHelper() {}
+
+  LabelType GetLabelValueForGivenNode( NodeType iNode )
+    {
+    return Superclass::Far;
+    }
+
+  void SetLabelValueForGivenNode( NodeType iNode, LabelType iLabel )
+    { (void) iNode; (void) iLabel; }
+
+  void UpdateNeighbors( NodeType iNode )
+    { (void) iNode; }
+
+  void CheckTopology( NodeType iNode )
+    { (void) iNode; }
+
+private:
+  FastMarchingBaseTestHelper( const Self& );
+  void operator = ( const Self& );
+};
+}
+
 int main( int argc, char* argv[] )
 {
-  typedef ImageFastMarchingTraits< 2, float, float >
+  typedef itk::ImageFastMarchingTraits< 2, float, float >
     ImageTraits;
-  typedef FastMarchingBase< ImageTraits > ImageFastMarching;
+  typedef itk::FastMarchingBaseTestHelper< ImageTraits > ImageFastMarching;
   ImageFastMarching::Pointer fmm = ImageFastMarching::New();
 
   return EXIT_SUCCESS;
